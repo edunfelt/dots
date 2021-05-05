f12cfcc020b7fe8941f27f61f031dfebd15b3328
edunfelt <edun@dunfelt.se>
2021-05-05 19:44:05 +0200

    PyRadio configs added

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
+ '(org-agenda-files
+   '("/media/nas/home/00-09_Meta/01_Emacs/01.01_Org/journal/2021-18-0503.org"))
  '(org-journal-date-format "%A, %d")
  '(org-journal-date-prefix "* ")
  '(org-journal-dir "/media/nas/home/00-09_Meta/01_Emacs/01.01_Org/journal/")
